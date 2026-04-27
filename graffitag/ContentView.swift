b1fdddc30a7a31eae27ab2a566358b1b8642d813
Rafael Ortiz <rafael007@gmail.com>
2026-04-27 01:41:42 -0500

    Initial Commit

---
+++ graffitag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  graffitag
+//
+//  Created by Rafael Ortiz on 27/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ graffitag/graffitagApp.swift
@@ -0,0 +1,17 @@
+//
+//  graffitagApp.swift
+//  graffitag
+//
+//  Created by Rafael Ortiz on 27/04/26.
+//
+
+import SwiftUI
+
+@main
+struct graffitagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
